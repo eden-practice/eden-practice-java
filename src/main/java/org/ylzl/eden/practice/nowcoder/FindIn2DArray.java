48cc7cd06307705488cd9951ab9ac254c164ccc5
梦想歌 <shiyindaxiaojie@gmail.com>
2020-05-07 00:46:49 +0800

    add code

---
+++ src/main/java/org/ylzl/eden/practice/nowcoder/FindIn2DArray.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ylzl.eden.practice.nowcoder;
+
+/**
+ * 二维数组中的查找
+ *
+ * <p>给定一个二维数组，其每一行从左到右递增排序，从上到下也是递增排序。给定一个数，判断这个数是否在该二维数组中。
+ * <p>要求时间复杂度 O(M + N)，空间复杂度 O(1)。其中 M 为行数，N 为 列数。</p>
+ * {@code
+ * [
+ *   [1, 4, 7, 11, 15],
+ *   [2, 5, 8, 12, 19],
+ *   [3, 6, 9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]}
+ *
+ * @author gyl
+ * @since 1.0.0
+ */
+public class FindIn2DArray {
+
+
+}
